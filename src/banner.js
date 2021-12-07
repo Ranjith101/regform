f58a0c1e0939fef2d0a31784672e02f7f2a98f29
Ranjith <ranjithsundar10@rediffmail.com>
2021-12-07 11:12:24 +0530

    first commit

---
+++ src/banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Banner = () =>{
+    return(
+            <section id="mybanner">
+                React + JSON-SERVER API E-commerce Project   
+            </section>
+    )
+}
+
+export default Banner;
